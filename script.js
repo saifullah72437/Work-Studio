037134d5383389cf4561b757f52fb7b313f76ff5
Saifullah Khan <73275780+saifullah72437@users.noreply.github.com>
2024-03-07 22:47:46 -0800

    HTML, CSS, JS File added

---
+++ script.js
@@ -0,0 +1,58 @@
+function firstAnimation() {
+    var tl = gsap.timeline();
+tl.to(".yellow1", {
+    y: "-100%",
+    duration: 0.5,
+    delay: 1,
+    ease: "expo.out"
+})
+tl.to(".loader video", {
+    y: "-100%",
+    duration: 1,
+    ease: "expo.out"
+
+})
+tl.to(".loader .yellow2", {
+    opacity: 0,
+    delay:0.2,
+    ease: "expo.out"
+
+})
+tl.to(".loader", {
+    opacity:0,
+    
+})
+tl.to(".loader", {
+    display:"none",
+
+})
+}
+
+firstAnimation();
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('.main'),
+        smooth: true
+    });
+// });
+
+let part2 = document.querySelector(".part2")
+let elem = document.querySelectorAll(".elem")
+
+elem.forEach(element => {
+    element.addEventListener("mouseover", ()=>{
+        let img = element.getAttribute("data-img");
+       part2.style.backgroundImage = `url(${img})`
+    })
+});
+
+
+let backtotop = document.querySelectorAll(".backtotop")
+backtotop.forEach((e) =>{
+    e.addEventListener("click", ()=>{
+        console.log("hello");
+        scroll.scrollTo(0)
+    })
+})
